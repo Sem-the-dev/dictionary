fa21220e76c0e0d565a8f10e30f0219957d57090
sem-the-dev <semhar.tesfu@outlook.com>
2021-05-27 12:22:09 +0100

    add response data

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,32 @@ import React, { useState } from "react";
 import axios from 'axios'
 
 export default function Dictionary() {
-  const [keyword, setKeyword] = useState("");
+  const [searchWord, setSearchWord] = useState("");
+  const [responseData, setResponseData] = useState("")
+
 
   function searchDictionary(e) {
     e.preventDefault();
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`;
     axios.get(apiUrl).then(handleResponse);
     alert(keyword);
   }
   function inputValue(e) {
-    setKeyword(e.target.value);
+    setSearchWord(e.target.value);
   }
 
   function handleResponse(response){
-    console.log(response.data)
+      setResponseData({
+        word: response.data[0].word,
+        phonetics: response.data[0].phonetics[0].text,
+        definition1: response.data[0].meanings[0].definitions[0].definition,
+        wordType1: response.data[0].meanings[0].partOfSpeech,
+        definition2: response.data[0].meanings[1].definitions[0].definition,
+        wordType2: response.data[0].meanings[1].partOfSpeech,
+      });
+     console.log(response.data)
+    // console.log(response.data[0].meanings[0].partOfSpeech);
+   
   }
 
 
@@ -24,7 +36,19 @@ export default function Dictionary() {
       <form onSubmit={searchDictionary}>
         <input type="search" autoFocus={true} onChange={inputValue} />
       </form>
-      <h3>{keyword}</h3>
+      <h3>{responseData.word}</h3>
+      <p style={{ "font-style": "italic" }}>{responseData.phonetics}</p>
+      <ol>
+        <li>
+          {responseData.wordType1}: {responseData.definition1}
+        </li>
+        <br />
+        <li>
+          {responseData.wordType2}
+          : {responseData.definition2}
+        </li>
+      </ol>
+      <p> </p>
     </div>
   );
 }
